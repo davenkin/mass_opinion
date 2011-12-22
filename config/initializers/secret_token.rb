368c4d741ef1106f2a58d9fcdf431d22ed3cca85
Teng Yun <yeng@thoughtworks.com>
2011-12-22 11:37:09 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassOpinion::Application.config.secret_token = 'b406bce86e411bd0b08f8010e49bb92876fbd81e0b94b06f42513c712fc5562b03aaa102edebcc2bbc73f11ca221cc2fedaed6f4672c8f51f7697c21d4fb099f'
