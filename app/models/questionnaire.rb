98dbf35ed70c220756256cb5dd6b75004a68f33b
Davenkin <davenkin@163.com>
2012-01-17 16:49:26 +0800

    delete questionnaire model

--- app/models/questionnaire.rb
+++
@@ -1,6 +0,0 @@
-class Questionnaire < ActiveRecord::Base
-  belongs_to :user
-  has_many :questions, :dependent => :destroy
-  has_many :comments, :dependent => :destroy
-  has_and_belongs_to_many :categories
-end
